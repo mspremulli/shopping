cd95f1e34117c1e0b85925368e54fe9dac94a902
Michael Spremulli <michaels.wpi@gmail.com>
2023-12-05 10:26:17 -0500

    removed empty file

--- shopping.py
+++ shopping.py
@@ -32,60 +32,101 @@ def main():
 
 
 def load_data(filename):
-    """
-    Load shopping data from a CSV file `filename` and convert into a list of
-    evidence lists and a list of labels. Return a tuple (evidence, labels).
+  evidence = []
+  labels = []
+  
+  month_mapping = {
+          'Jan': 0,
+          'Feb': 1,
+          'Mar': 2,
+          'Apr': 3,
+          'May': 4,
+          'Jun': 5,
+          'Jul': 6,
+          'Aug': 7,
+          'Sep': 8,
+          'Oct': 9,
+          'Nov': 10,
+          'Dec': 11
+        }
 
-    evidence should be a list of lists, where each list contains the
-    following values, in order:
-        - Administrative, an integer
-        - Administrative_Duration, a floating point number
-        - Informational, an integer
-        - Informational_Duration, a floating point number
-        - ProductRelated, an integer
-        - ProductRelated_Duration, a floating point number
-        - BounceRates, a floating point number
-        - ExitRates, a floating point number
-        - PageValues, a floating point number
-        - SpecialDay, a floating point number
-        - Month, an index from 0 (January) to 11 (December)
-        - OperatingSystems, an integer
-        - Browser, an integer
-        - Region, an integer
-        - TrafficType, an integer
-        - VisitorType, an integer 0 (not returning) or 1 (returning)
-        - Weekend, an integer 0 (if false) or 1 (if true)
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  
+  with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        
+        for row in reader:
+          administrative = int(row[0])
+          administrative_duration = float(row[1])
+          informational = int(row[2])
+          informational_duration = float(row[3])
+          product_related = int(row[4])
+          product_related_duration = float(row[5])
+          bounce_rates = float(row[6])
+          exit_rates = float(row[7])
+          page_values = float(row[8])
+          special_day = float(row[9])
+          month = [row[10]]
+          operating_systems = int(row[11])
+          browser = int(row[12])
+          region = int(row[13])
+          traffic_type = int(row[14])
+          visitor_type = 1 if row[15] else 0
+          weekend =  1 if row[16] else 0
 
-    labels should be the corresponding list of labels, where each label
-    is 1 if Revenue is true, and 0 otherwise.
-    """
-    raise NotImplementedError
-
+        month = list(map(lambda month: month_mapping[month], months))
+          
+        evidence.append([
+          administrative,
+          administrative_duration,
+          informational,
+          informational_duration,
+          product_related,
+          product_related_duration,
+          bounce_rates,
+          exit_rates,
+          page_values,
+          special_day,
+          month, 
+          operating_systems,
+          browser, 
+          region, 
+          traffic_type,
+          visitor_type,
+          weekend
+        ])
+          
+        if row[17] == 'TRUE': 
+          label = 1 
+        else:
+          label = 0
+        labels.append(label)
+        
+            
+  return (evidence, labels);
 
 def train_model(evidence, labels):
-    """
-    Given a list of evidence lists and a list of labels, return a
-    fitted k-nearest neighbor model (k=1) trained on the data.
-    """
-    raise NotImplementedError
+    k=1
+    neighboor = KNeighborsClassifier(n_neighbors=k)
+    neighboor.fit(evidence, labels)
+    return neighboor
 
 
 def evaluate(labels, predictions):
-    """
-    Given a list of actual labels and a list of predicted labels,
-    return a tuple (sensitivity, specificity).
-
-    Assume each label is either a 1 (positive) or 0 (negative).
-
-    `sensitivity` should be a floating-point value from 0 to 1
-    representing the "true positive rate": the proportion of
-    actual positive labels that were accurately identified.
-
-    `specificity` should be a floating-point value from 0 to 1
-    representing the "true negative rate": the proportion of
-    actual negative labels that were accurately identified.
-    """
-    raise NotImplementedError
+  guess_positive = 0
+  actual_positive = 0
+  guess_negitive = 0
+  actual_negitive = 0
+  
+  for index in range(len(labels)):
+    if labels[index] == 1: actual_positive +=1 
+    if predictions[index] == 1: guess_positive +=1
+  
+    if labels[index] == 0: actual_positive +=1 
+    if predictions[index] == 0: guess_positive +=1
+    
+  return (guess_positive/actual_positive, guess_negitive/actual_negitive)
 
 
 if __name__ == "__main__":
